00c9b0256e38da115e6a6805346f30e0bc41461a
aichichang <aichi.chang@krakenflex.com>
2023-09-18 08:06:21 +0100

    feat: change about information

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -7,9 +7,19 @@
 
 <h1 class="text-3xl font-bold font-serif my-6">Hey, it's Aichi</h1>
 <div class="w-9/12 mx-auto">
-	<p class="text-xl text-slate-600 font-sans mb-4">
-		Full-stack software engineer currently working at <span class="font-bold">Nando's</span>
+	<p class="text-xl text-slate-600 font-sans mb-6">
+		Full-stack software engineer currently working at <span class="font-bold">Kraken Flex 🐙</span>
 	</p>
+  <p class="text-xl text-slate-600 font-sans">
+    Things I'm interested in:
+  </p>
+  <div class="pl-16">
+    <ul class="list-disc list-inside text-xl text-slate-600 font-sans text-left">
+      <li class='text-base list-none'> NeoVim</li>
+      <li class='text-base list-none'> Typescript,Rust,Go</li>
+      <li class='text-base list-none'> ACG ٩(ˊᗜˋ*)و ♡</li>
+    </ul>
+  </div>
 </div>
 
 <div class="absolute bottom-10 inset-x-0">
